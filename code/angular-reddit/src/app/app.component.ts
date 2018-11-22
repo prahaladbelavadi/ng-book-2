370fe7c32eec2e5a07707316ead46b885009bb24
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-11-23 02:50:18 +0530

    push new inputs to article array

--- code/angular-reddit/src/app/app.component.ts
+++ code/angular-reddit/src/app/app.component.ts
@@ -10,11 +10,21 @@ import { Article } from './article/article.model';
 export class AppComponent {
 articles: Article[];
 
-constructor(){
-  this.articles = [
-    new Article('Angular 1','http://angular.io',3),
-    new Article('Angular 2', 'http://angular.io', 2),
-    new Article('Angular 3', 'http://angular.io', 1),
-  ]
+  constructor() {
+    this.articles = [
+      new Article('Angular 1', 'http://angular.io', 3),
+      new Article('Angular 2', 'http://angular.io', 2),
+      new Article('Angular 3', 'http://angular.io', 1),
+    ]
+  }
+
+  addArticle(title: HTMLInputElement, link:HTMLInputElement) : boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
 }
+
+
 }
