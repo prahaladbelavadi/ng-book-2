52c0dd2d8b5b580cdbe96c0e27ce6f8f02d0e029
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-11-23 02:43:24 +0530

    rendering a list of articles

--- code/angular-reddit/src/app/app.component.ts
+++ code/angular-reddit/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  addArticle(title: HTMLInputElement, link:HTMLInputElement) : boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    return false;
+articles: Article[];
+
+constructor(){
+  this.articles = [
+    new Article('Angular 1','http://angular.io',3),
+    new Article('Angular 2', 'http://angular.io', 2),
+    new Article('Angular 3', 'http://angular.io', 1),
+  ]
 }
 }
--- code/angular-reddit/src/app/article/article.component.ts
+++ code/angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Article } from './article.model'
 
@@ -10,15 +10,9 @@ import { Article } from './article.model'
 
 export class ArticleComponent implements OnInit {
 @HostBinding('attr.class') cssClass = 'row'
-article:Article;
+@Input() article: Article;
 
-  constructor() { 
-    this.article = new Article(
-      'Angular 2',
-      'http://amgular.io',
-      10
-    )
-  }
+  constructor() {}
 
   voteUp(): boolean {
     this.article.voteUp();
