9db0f48787412fe8da6436000673da4ae15a4e69
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-11-22 17:20:51 +0530

    add article component into app componentl, tie votes aand prevent reloading when returned

--- code/angular-reddit/src/app/article/article.component.ts
+++ code/angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-article',
@@ -6,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+@HostBinding('attr.class') cssClass = 'row'
+votes: number
+title: string
+link: string
+  constructor() { 
+    this.title = 'Angular 2';
+    this.link = 'https://angular.io';
+    this.votes = 10;
+  }
 
-  constructor() { }
+  voteUp(){
+    this.votes = this.votes+1
+    return false;
+  }
+
+  voteDown(){
+    this.votes -= 1
+    return false;
+  }
 
   ngOnInit() {
   }
