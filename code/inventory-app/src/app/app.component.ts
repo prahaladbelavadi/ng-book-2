1c0c333db84ce1d1f431430c6ceaac50eb5c28f9
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-12-07 16:10:37 +0530

    add product model.ts and modify app.component.ts

--- code/inventory-app/src/app/app.component.ts
+++ code/inventory-app/src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
+products: Product[];
+
+constructor(){
+  this.products = [
+    new this.products(
+      'MYSHOES',
+      'Black Running Shoes',
+      '/assets/images/products/black-shoes.jpg',
+      ['Men', 'Shoes', 'Running Shoes'],
+      109.99
+    ),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+      ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+      ['Men', 'Accessories', 'Hats'],
+        29.99)
+]
+  ]
+
+
+  productWasSelected(product: Product) void{
+    console.log('Product Clicked', product);  
+  }
+}
+
 }
